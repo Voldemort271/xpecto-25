219ed9106f3efbdcf2458cfb6bee96875e41be8f
Shubham S Padhi <shubhamspadhi@gmail.com>
2024-12-18 11:25:31 +0530

    About page started

---
+++ src/components/(newspaper)/navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewspaperNavbar = () => {
+  return (
+    <div className="relative flex w-screen flex-col items-center bg-red-400 px-12 py-5">
+      Glory to the wonderful navigation
+    </div>
+  );
+};
+
+export default NewspaperNavbar;
