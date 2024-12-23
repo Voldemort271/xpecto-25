d466f6616fd22eae6b309d5c7fad869eab6ff68c
riddhimaheshwari <riddhim1712@gmail.com>
2024-12-23 13:02:41 +0530

    Update page.tsx

--- src/app/(dystopian)/competitions/create/page.tsx
+++ src/app/(dystopian)/competitions/create/page.tsx
@@ -2,11 +2,16 @@
 
 import React, { useEffect } from "react";
 import { api } from "@/trpc/react"; // Import the api object
-import {CompetitionType} from "../../../types"
-import { useCurrentUser } from "@/lib/utils";
+import {CompetitionType, MultiEntitySchemaType} from "../../../types"
+import { competitionLevelSchema, useCurrentUser, problemStatementSchema,ruleSchema } from "@/lib/utils";
 import CreateTeamDialog from "@/components/(dystopian)/create-team-dialog";
 import { Button } from "@/components/ui/button"; // Import the Button component
-import Link from 'next/link'
+import Links from "../../../../components/ui/Links";
+import MultiEntity from "../../../../components/ui/MultiEntity";
+
+
+
+
 const Page = () => {
 
 
@@ -55,12 +60,49 @@ const Page = () => {
   venue:""
  });
 
+ const competitionLevelData=[
+  {name:'Round-1',description:'Round-1 is very important', venue:'college room-1',timeline:'10/01/2025'},
+  {name:'Round-2',description:'Round-2 is logical', venue:'college room-2',timeline:'10/02/2025'},
+  {name:'Round-3',description:'Round-3 is final round', venue:'Other place',timeline:'10/03/2025'},
+  {id:999999,name:'',description:'', venue:'',timeline:''}
+
+ ]
+
+ const problemStatementData=[
+  {name:'problem-1',description:'problem-1 statement'},
+  {name:'problem-2',description:'problem-2 statement'},
+  {id:999999,name:'',description:''}
+ ]
+
+ const rulesData=[
+  {name:'rule-1'},
+  {name:'rule-2'},
+  {id:999999,name:''}
+ ]
+
+
+
+ const [selectedTab, setSelectedTab]=React.useState(1);
   
 
   return (
     <>
-      <div className="flex flex-col">
-      <div className="flex flex-row">
+    <Links links={[{id:1,text:'Details'},{id:2,text:'Levels'},{id:3,text:'Problem Statement'},{id:4,text:'Rules'}]} setTab={(id:number)=>setSelectedTab(id)} ></Links>
+     
+     {selectedTab==2 && <div style={{padding:'1rem'}}>
+      <MultiEntity entityData={competitionLevelData} schema={competitionLevelSchema}></MultiEntity> 
+      </div>}
+
+      {selectedTab==3 && <div style={{padding:'1rem'}}>
+      <MultiEntity entityData={problemStatementData} schema={problemStatementSchema}></MultiEntity> 
+      </div>}
+
+      {selectedTab==4 && <div style={{padding:'1rem'}}>
+      <MultiEntity entityData={rulesData} schema={ruleSchema}></MultiEntity> 
+      </div>}
+
+     {selectedTab==1 && <div className="flex flex-col" style={{padding:'1rem'}}>
+      <div className="flex flex-row" >
         <div className="left-col">
           <label className="font-semibold">
             Name
@@ -188,7 +230,7 @@ const Page = () => {
                 } style={{marginLeft:"10.5rem",marginTop:"1rem"}}>Create</button>
         <button className="btn" style={{marginLeft:"1rem",marginTop:"1rem"}} onClick={()=>{window.location.href="/competitions"}}>Cancel</button>
         </div>
-      </div>
+      </div>}
     </>
   );
 };
