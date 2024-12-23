023937aeb25ba4b3f5a07f72996677bb2a95ac00
mrhello291 <hoda.asif123@gmail.com>
2024-12-23 19:35:30 +0530

    schema fixed

--- src/lib/context.tsx
+++ src/lib/context.tsx
@@ -9,7 +9,7 @@ import React, {
 } from "react";
 import { useUser } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
-import type { User,Competition } from "@prisma/client";
+import type { User } from "@prisma/client";
 import { api } from "@/trpc/react"; // Import the api object
 
 
