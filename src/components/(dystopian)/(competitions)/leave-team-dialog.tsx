66d0268d5a434662a598bfefed790b5b00242233
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-01-03 10:45:50 +0530

    Leave team dialog done

--- src/components/(dystopian)/(competitions)/leave-team-dialog.tsx
+++ src/components/(dystopian)/(competitions)/leave-team-dialog.tsx
@@ -5,11 +5,15 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "../../ui/dialog";
-import { Button } from "../../ui/button";
 import { api } from "@/trpc/react";
 import { useCurrentUser } from "@/lib/utils";
 import type { TeamWithFullDetails } from "@/app/types";
 import { CursorContext } from "@/context/cursor-context";
+import { Handjet, Share_Tech } from "next/font/google";
+import MarqueeContainer from "@/components/(dystopian)/common/marquee-container";
+
+const handjet = Handjet({ subsets: ["latin"] });
+const sharetech = Share_Tech({ weight: "400", subsets: ["latin"] });
 
 const LeaveTeamDialog = ({ regTeam }: { regTeam: TeamWithFullDetails }) => {
   const { setIsHovered } = useContext(CursorContext);
@@ -49,15 +53,33 @@ const LeaveTeamDialog = ({ regTeam }: { regTeam: TeamWithFullDetails }) => {
           leave team
         </button>
       </DialogTrigger>
-      <DialogContent>
-        <DialogTitle>Leave Team</DialogTitle>
-        <div className="flex justify-start">
-          <Button
+      <DialogContent
+        className={`max-w-[800px] border-2 border-amber-50/[0.7] bg-neutral-900 p-0 text-amber-50 ${handjet.className} tracking-widest`}
+      >
+        <DialogTitle className="relative z-10 flex h-12 w-full cursor-none items-center overflow-clip border-b-2 border-amber-50/[0.7] bg-neutral-900 text-2xl font-normal uppercase tracking-wider text-amber-50">
+          <MarqueeContainer
+            text={[
+              "confirm choice",
+              "leave team",
+              "confirm choice",
+              "leave team",
+            ]}
+          />
+        </DialogTitle>
+        <div
+          className={`${sharetech.className} w-full p-5 text-lg tracking-tight`}
+        >
+          Are you sure? This action is irreversible, and you will leave this
+          team with immediate effect.
+        </div>
+        <div className="flex justify-end">
+          <button
+            type="submit"
             onClick={() => handleUserTeamDelete({ teamId: regTeam.id })}
-            className="bg-red-500 hover:bg-red-200"
+            className="bg-amber-50/[0.7] px-5 py-2 text-2xl font-normal uppercase text-neutral-900"
           >
-            Yes I want out of Team
-          </Button>
+            leave team
+          </button>
         </div>
       </DialogContent>
     </Dialog>
