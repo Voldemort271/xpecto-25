4bdb7ed824a1bbf0a973623689ba06dca22cc36f
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-01-06 10:16:17 +0530

    Fixed route specificity

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return <div>hehe {children}</div>;
+};
+
+export default AuthLayout;
--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,31 @@
-import { SignIn } from "@clerk/nextjs";
+"use client";
+
+import { SignIn, useSignIn } from "@clerk/nextjs";
 import React from "react";
+import type { OAuthStrategy } from "@clerk/types";
 
 const Signin = () => {
+  const { signIn } = useSignIn();
+
+  if (!signIn) return null;
+
+  const signInWith = (strategy: OAuthStrategy) => {
+    return signIn.authenticateWithRedirect({
+      strategy,
+      redirectUrl: "/sign-up/sso-callback",
+      redirectUrlComplete: "/",
+    });
+  };
+
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center gap-10">
       <SignIn />
+      <button onClick={() => signInWith("oauth_google")}>
+        Sign in with Google
+      </button>
+      <button onClick={() => signInWith("oauth_github")}>
+        Sign in with GitHub
+      </button>
     </main>
   );
 };
