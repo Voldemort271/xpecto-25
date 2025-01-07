0610424dd4981f788a13c2ab198050b803aca506
Riddhi Maheshwari <riddhim1712@gmail.com>
2025-01-07 12:32:48 +0530

    update loader

--- src/app/(dystopian)/competitions/page.tsx
+++ src/app/(dystopian)/competitions/page.tsx
@@ -5,6 +5,7 @@ import CompCard from "@/components/(dystopian)/(competitions)/competition-card";
 import { motion } from "motion/react";
 import MarqueeContainer from "@/components/(dystopian)/common/marquee-container";
 import SectionHeader from "@/components/(dystopian)/common/section-header";
+import Loader from "@/components/(dystopian)/common/loader";
 const Page = () => {
   const { data: competitions, isLoading } =
     api.competition.getCompetitions.useQuery();
@@ -21,16 +22,7 @@ const Page = () => {
       <SectionHeader title="competitions">prove your mettle</SectionHeader>
       <div className="grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
 
-        {isLoading && (
-          <div className="overlay">
-            <img
-              className="loader"
-              src="/loading.gif"
-              width="190rem"
-              height="190rem"
-            ></img>
-          </div>
-        )}
+        {isLoading && <Loader/>}
         {competitions
           ?.sort((a, b) =>
             a.competitionDetails.begin_time > b.competitionDetails.begin_time
