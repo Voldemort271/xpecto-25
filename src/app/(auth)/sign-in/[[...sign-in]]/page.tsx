89eff4f8fb992fc9db484742096ef2ecd76441a7
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-01-07 10:36:51 +0530

    Sign in page done

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,15 +1,30 @@
 "use client";
 
-import { SignIn, useSignIn } from "@clerk/nextjs";
-import React from "react";
-import type { OAuthStrategy } from "@clerk/types";
+import { useSignIn } from "@clerk/nextjs";
+import React, { type FormEvent } from "react";
+import type {
+  EmailCodeFactor,
+  OAuthStrategy,
+  SignInFirstFactor,
+} from "@clerk/types";
 import MarqueeContainer from "@/components/(dystopian)/common/marquee-container";
 import GoogleLogo from "@/components/(auth)/google";
 import GithubLogo from "@/components/(auth)/github";
+import { useRouter } from "next/navigation";
+import { Share_Tech } from "next/font/google";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+const sharetech = Share_Tech({ weight: "400", subsets: ["latin"] });
 
 const SigninPage = () => {
-  const { signIn } = useSignIn();
-  if (!signIn) return null;
+  const { isLoaded, setActive, signIn } = useSignIn();
+  const [verifying, setVerifying] = React.useState(false);
+  const [email, setEmail] = React.useState("");
+  const [code, setCode] = React.useState("");
+  const router = useRouter();
+
+  if (!isLoaded && !signIn) return null;
 
   const signInWith = (strategy: OAuthStrategy) => {
     return signIn.authenticateWithRedirect({
@@ -19,8 +34,71 @@ const SigninPage = () => {
     });
   };
 
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    if (!isLoaded && !signIn) return null;
+
+    try {
+      const { supportedFirstFactors } = await signIn.create({
+        identifier: email,
+      });
+
+      const isEmailCodeFactor = (
+        factor: SignInFirstFactor,
+      ): factor is EmailCodeFactor => {
+        return factor.strategy === "email_code";
+      };
+      const emailCodeFactor = supportedFirstFactors?.find(isEmailCodeFactor);
+
+      if (emailCodeFactor) {
+        const { emailAddressId } = emailCodeFactor;
+
+        await signIn.prepareFirstFactor({
+          strategy: "email_code",
+          emailAddressId,
+        });
+
+        setVerifying(true);
+      }
+    } catch (err) {
+      setEmail("");
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      alert(err.errors[0].longMessage);
+      // console.error("Error:", JSON.stringify(err, null, 2));
+    }
+  };
+
+  const handleVerification = async (e: FormEvent) => {
+    e.preventDefault();
+    if (!isLoaded && !signIn) return null;
+
+    try {
+      const signInAttempt = await signIn?.attemptFirstFactor({
+        strategy: "email_code",
+        code,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+
+        router.push("/");
+      } else {
+        console.error(signInAttempt);
+      }
+    } catch (err) {
+      setCode("");
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      alert(err.errors[0].longMessage);
+      // console.error("Error:", JSON.stringify(err, null, 2));
+    }
+  };
+
   return (
-    <main className="relative flex min-w-[800px] flex-col items-center justify-center overflow-clip border-2 border-amber-50/[0.7] bg-neutral-900">
+    <main className="relative flex max-w-[800px] flex-col items-center justify-center overflow-clip border-2 border-amber-50/[0.7] bg-neutral-900 md:min-w-[800px]">
       <div className="relative z-10 flex h-12 w-full items-center overflow-clip border-b-2 border-amber-50/[0.7] bg-neutral-900 text-2xl font-normal uppercase tracking-wider text-amber-50">
         <MarqueeContainer
           text={[
@@ -52,8 +130,82 @@ const SigninPage = () => {
         <div className="text-xl font-light uppercase text-amber-50">OR</div>
         <div className="h-[2px] w-full bg-amber-50/[0.7]"></div>
       </div>
+      <div className="relative w-full">
+        {verifying ? (
+          <form
+            onSubmit={handleVerification}
+            className="flex w-full flex-col items-center justify-center gap-5 pt-5"
+          >
+            <div className="flex w-full items-center justify-center gap-2.5 px-5">
+              <Label
+                htmlFor="code"
+                className="text-2xl font-normal uppercase text-amber-50"
+              >
+                Enter your verification code
+              </Label>
+              <Input
+                value={code}
+                id="code"
+                name="code"
+                className={`max-w-sm rounded-none border-2 border-amber-50 text-lg text-amber-50 ${sharetech.className} tracking-tight`}
+                onChange={(e) => setCode(e.target.value)}
+              />
+            </div>
 
-      <SignIn />
+            <div className="flex w-full items-end justify-between gap-2.5">
+              <div
+                className={`${sharetech.className} p-5 tracking-tight text-amber-50/[0.5]`}
+              >
+                The code is valid for 5 minutes. You will be redirected to the
+                home page on successful sign in.
+              </div>
+              <button
+                type="submit"
+                className="bg-amber-50/[0.7] px-5 py-2.5 text-2xl font-normal uppercase text-neutral-900"
+              >
+                Verify
+              </button>
+            </div>
+          </form>
+        ) : (
+          <form
+            onSubmit={handleSubmit}
+            className="flex w-full flex-col items-center justify-center gap-5 pt-5"
+          >
+            <div className="flex w-full items-center justify-center gap-2.5 px-5">
+              <Label
+                htmlFor="email"
+                className="text-2xl font-normal uppercase text-amber-50"
+              >
+                Enter email
+              </Label>
+              <Input
+                value={email}
+                id="email"
+                name="email"
+                type="email"
+                className={`max-w-sm rounded-none border-2 border-amber-50 text-lg text-amber-50 ${sharetech.className} tracking-tight`}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="flex w-full items-end justify-between gap-2.5">
+              <div
+                className={`${sharetech.className} p-5 tracking-tight text-amber-50/[0.5]`}
+              >
+                Preferably use your institute email ID. You will receive a
+                verification code to sign in to your account.
+              </div>
+              <button
+                type="submit"
+                className="bg-amber-50/[0.7] px-5 py-2.5 text-2xl font-normal uppercase text-neutral-900"
+              >
+                Continue
+              </button>
+            </div>
+          </form>
+        )}
+      </div>
+      {/*<SignIn />*/}
     </main>
   );
 };
