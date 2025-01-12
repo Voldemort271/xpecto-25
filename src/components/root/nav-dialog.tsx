ad5e3e2b31adb844b9a72750ef7f573e83329f51
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-01-12 15:31:33 +0530

    Nav screen done

--- src/components/root/nav-dialog.tsx
+++ src/components/root/nav-dialog.tsx
@@ -1,16 +1,11 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { motion } from "motion/react";
 import { Inter } from "next/font/google";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import { sleep } from "@/lib/utils";
 import CustomToast from "@/components/custom-toast";
 import { toast } from "sonner";
@@ -23,19 +18,50 @@ interface Props {
 
 const inter = Inter({ subsets: ["latin"] });
 
+const pageLinks = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  {
+    name: "Competitions",
+    href: "/competitions",
+  },
+  { name: "Expos", href: "/expos" },
+  { name: "Pronites", href: "/pronites" },
+  { name: "Sponsors", href: "/sponsors" },
+];
+
 const NavDialog = ({ toggle, setToggle }: Props) => {
   const router = useRouter();
+  const [override, setOverride] = useState(false);
 
   const handleSubmit = async (e: FormData) => {
     console.log(e);
-    toast.custom((t) => (
-      <CustomToast variant="info" metadata={t}>
-        Warping to {e.get("location")?.toString()}. Please standby.
-      </CustomToast>
-    ));
-    await sleep(1000);
-    router.push(e.get("location")?.toString() ?? "/");
-    setToggle(!toggle);
+    if (override) {
+      toast.custom(
+        (t) => (
+          <CustomToast variant="warning" metadata={t}>
+            Proceeding to authorization screen. Please standby.
+          </CustomToast>
+        ),
+        { duration: 1000, position: "top-right" },
+      );
+      router.push("/sign-in");
+      setToggle(!toggle);
+    } else {
+      if (e.get("location")) {
+        toast.custom(
+          (t) => (
+            <CustomToast variant="warning" metadata={t}>
+              Warping to {e.get("location")?.toString()}. Please standby.
+            </CustomToast>
+          ),
+          { duration: 1000, position: "top-right" },
+        );
+        await sleep(1000);
+        router.push(e.get("location")?.toString() ?? "/");
+        setToggle(!toggle);
+      }
+    }
   };
 
   return (
@@ -67,7 +93,7 @@ const NavDialog = ({ toggle, setToggle }: Props) => {
           travel technology. Please select the appropriate options and submit to
           warp to your destination. Brought to you by Xpecto &apos;25.
         </div>
-        <form action={handleSubmit} className="flex w-full flex-col gap-2">
+        <form className="flex w-full flex-col gap-2" action={handleSubmit}>
           <div className="flex items-center gap-2">
             <Label htmlFor="name" className="text-slate-700">
               Name
@@ -75,7 +101,7 @@ const NavDialog = ({ toggle, setToggle }: Props) => {
             <Input
               type="text"
               name="name"
-              placeholder="Enter traveller's name."
+              placeholder="Enter name of subject"
               className="border-slate-300 bg-slate-100 transition-all focus:border-2 focus:border-blue-400"
             />
           </div>
@@ -86,118 +112,54 @@ const NavDialog = ({ toggle, setToggle }: Props) => {
             <div className="w-32 shrink-0 text-sm font-medium text-slate-700">
               Select destination
             </div>
-            <RadioGroup
-              defaultValue="option-one"
-              name="location"
-              className="grid w-full grid-cols-2"
+            <div className="grid w-full grid-cols-2 gap-2">
+              {pageLinks.map((el, i) => (
+                <div className="flex items-center gap-2" key={i}>
+                  <Input
+                    type="radio"
+                    name="location"
+                    value={el.href}
+                    id={el.name}
+                    className="h-4 w-4 cursor-pointer align-middle"
+                    disabled={override}
+                  />
+                  <Label
+                    htmlFor={el.name}
+                    className={
+                      override
+                        ? "cursor-not-allowed text-slate-500"
+                        : "cursor-pointer text-slate-800"
+                    }
+                  >
+                    {el.name}
+                  </Label>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="mt-5 flex items-center justify-start gap-2">
+            <Input
+              type="checkbox"
+              name="override"
+              id="override"
+              className="h-4 w-4 align-middle"
+              checked={override}
+              onChange={(e) => setOverride(e.target.checked)}
+            />
+            <Label
+              htmlFor="override"
+              className={override ? "text-red-600" : "text-slate-700"}
             >
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem
-                  value="/"
-                  id="home"
-                  className="focus:ring-2 focus:ring-blue-600"
-                />
-                <Label htmlFor="/">
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger>Home</TooltipTrigger>
-                      <TooltipContent>
-                        <p>Home</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem
-                  value="/about"
-                  id="about"
-                  className="focus:ring-2 focus:ring-blue-600"
-                />
-                <Label htmlFor="/about">
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger>About</TooltipTrigger>
-                      <TooltipContent>
-                        <p>About</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem
-                  value="/competitions"
-                  id="competitions"
-                  className="focus:ring-2 focus:ring-blue-600"
-                />
-                <Label htmlFor="/competitions">
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger>Competitions</TooltipTrigger>
-                      <TooltipContent>
-                        <p>Competitions</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem
-                  value="/expos"
-                  id="expos"
-                  className="focus:ring-2 focus:ring-blue-600"
-                />
-                <Label htmlFor="/expos">
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger>Expos</TooltipTrigger>
-                      <TooltipContent>
-                        <p>Expos</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem
-                  value="/pronites"
-                  id="pronites"
-                  className="focus:ring-2 focus:ring-blue-600"
-                />
-                <Label htmlFor="/pronites">
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger>Pronites</TooltipTrigger>
-                      <TooltipContent>
-                        <p>Pronites</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem
-                  value="/sponsors"
-                  id="sponsors"
-                  className="focus:ring-2 focus:ring-blue-600"
-                />
-                <Label htmlFor="/sponsors">
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger>Sponsors</TooltipTrigger>
-                      <TooltipContent>
-                        <p>Sponsors</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Label>
-              </div>
-            </RadioGroup>
+              Safety protocols: {override ? "Overridden" : "Intact"}
+            </Label>
+          </div>
+          <div className="mb-2 text-left text-xs font-medium text-slate-600">
+            Checking this option will take you to the authorization screen.
+            Proceed with caution.
           </div>
           <Button
             type="submit"
-            className="mt-5 w-fit bg-gradient-to-b from-blue-500 to-blue-600 px-5 py-2 text-amber-50 shadow-md shadow-neutral-900/[0.3]"
+            className="mt-2 w-fit bg-gradient-to-b from-blue-500 to-blue-600 px-5 py-2 text-amber-50 shadow-md shadow-neutral-900/[0.3]"
           >
             Submit
           </Button>
