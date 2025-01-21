f3b349204423729b3b551c0a5c874170938f6b3d
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-01-21 15:21:42 +0530

    Deployment fix

--- src/app/(dystopian)/test/page.tsx
+++ src/app/(dystopian)/test/page.tsx
@@ -1,18 +1,7 @@
 import React from "react";
-import PronitesHeader from "@/components/(dystopian)/pronites/pronites-header";
-import PronitesDisplay from "@/components/(dystopian)/pronites/pronites-display";
 
 const TestPage = () => {
-  return (
-    <div className="grid min-h-screen w-screen grid-rows-[64px_auto] bg-neutral-900 md:grid-cols-[64px_auto] md:grid-rows-1">
-      <div className="h-full w-full bg-neutral-900">
-        <PronitesHeader />
-      </div>
-      <div className="relative h-full w-full bg-neutral-900">
-        <PronitesDisplay />
-      </div>
-    </div>
-  );
+  return <div>hi</div>;
 };
 
 export default TestPage;
