ee7045393d06420f0c597165c6f9572f44465641
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-01-24 20:30:35 +0530

    Started sponsors page

--- src/app/(dystopian)/sponsors/page.tsx
+++ src/app/(dystopian)/sponsors/page.tsx
@@ -5,46 +5,51 @@ import React from "react";
 import { api } from "@/trpc/react";
 import Image from "next/image";
 import dummyImage from "@/../public/spon_logos/300.png";
+import SponsorTitle from "@/components/(dystopian)/sponsors/sponsor-title";
 
 const Page = () => {
   const allSpons = api.sponsor.getSponsor.useQuery({ id: "" }).data; // id="" fetches all spons
 
   return (
-    <div style={{ paddingTop: 200, paddingLeft: 100 }}>
-      <Link
-        href="/sponsors/create"
-        style={{ border: "1px solid white", padding: 2 }}
-      >
-        Create sponsor
-      </Link>
-      <br />
-      <br />
-      OUR SPONSORS 👇
-      <br />
-      <br />
-      <div style={{ display: "flex" }}>
-        {allSpons?.map((spon, i) => {
-          const sponSlug = spon.name.replaceAll(" ", "-");
-          return (
-            <div key={i} style={{ padding: 10 }}>
-              <Image src={dummyImage} alt="logo" width={100} height={100} />
+    <div className="w-full bg-neutral-900">
+      <SponsorTitle />
+      <div className="w-full bg-neutral-900 p-12">hi</div>
+      <div style={{ paddingTop: 200, paddingLeft: 100 }}>
+        <Link
+          href="/sponsors/create"
+          style={{ border: "1px solid white", padding: 2 }}
+        >
+          Create sponsor
+        </Link>
+        <br />
+        <br />
+        OUR SPONSORS 👇
+        <br />
+        <br />
+        <div style={{ display: "flex" }}>
+          {allSpons?.map((spon, i) => {
+            const sponSlug = spon.name.replaceAll(" ", "-");
+            return (
+              <div key={i} style={{ padding: 10 }}>
+                <Image src={dummyImage} alt="logo" width={100} height={100} />
 
-              <Link
-                style={{ border: "1px solid white", margin: 1 }}
-                href={"/sponsors/" + sponSlug}
-              >
-                {spon.name}
-              </Link>
-              <br />
-              <em>Events : </em>
-              <ul>
-                {spon.events.map((eve, k) => {
-                  return <li key={k}>{eve.name}</li>;
-                })}
-              </ul>
-            </div>
-          );
-        })}
+                <Link
+                  style={{ border: "1px solid white", margin: 1 }}
+                  href={"/sponsors/" + sponSlug}
+                >
+                  {spon.name}
+                </Link>
+                <br />
+                <em>Events : </em>
+                <ul>
+                  {spon.events.map((eve, k) => {
+                    return <li key={k}>{eve.name}</li>;
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
