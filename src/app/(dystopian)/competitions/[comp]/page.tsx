349905067a8ede793606fb1f98a7698d835fb92c
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-01-26 12:52:14 +0530

    Fixed height of invitations section

--- src/app/(dystopian)/competitions/[comp]/page.tsx
+++ src/app/(dystopian)/competitions/[comp]/page.tsx
@@ -10,45 +10,18 @@ const Page = ({ params }: { params: Promise<{ comp: string }> }) => {
   const { CurrentUser } = useCurrentUser();
 
   const compSlug = use(params).comp;
-  const { data: comp, isLoading: isCompLoading } =
-    api.competition.getCompBySlug.useQuery({
-      slug: compSlug,
-    });
-  const { data: plan, isLoading: isRegistrationLoading } =
-    api.event.checkUserRegisteration.useQuery(
-      {
-        userId: CurrentUser?.id ?? "",
-        eventId: comp?.competitionDetailsId ?? "",
-      },
-      {
-        enabled: !!CurrentUser && !!comp,
-      },
-    );
-  const { data: regTeam, isLoading: isTeamLoading } =
-    api.team.findTeamOfUser.useQuery(
-      {
-        userId: CurrentUser?.id ?? "",
-        competitionId: comp?.id ?? "",
-      },
-      {
-        enabled: !!CurrentUser && !!comp,
-      },
-    );
-
-  const regStatus = !!plan;
+  const { data: comp, isLoading } = api.competition.getCompBySlug.useQuery({
+    slug: compSlug,
+  });
 
   return (
     <>
-      {(isCompLoading || isRegistrationLoading || isTeamLoading) && (
+      {isLoading && (
         <div className="loading flex h-screen w-screen flex-col justify-center bg-neutral-900"></div>
       )}
       {comp && (
         <div className="flex w-screen flex-col items-center justify-center gap-12 xl:flex-row">
-          <CompetitionDetailsBox
-            comp={comp}
-            regStatus={regStatus}
-            regTeam={regTeam}
-          />
+          <CompetitionDetailsBox comp={comp} />
         </div>
       )}
     </>
