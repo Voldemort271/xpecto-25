34c53a1a87017aa04ed0df592a6abf86187c31f0
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-01-26 11:32:05 +0530

    Added loading state to competitions individual pages

--- src/app/(dystopian)/competitions/[comp]/page.tsx
+++ src/app/(dystopian)/competitions/[comp]/page.tsx
@@ -10,32 +10,38 @@ const Page = ({ params }: { params: Promise<{ comp: string }> }) => {
   const { CurrentUser } = useCurrentUser();
 
   const compSlug = use(params).comp;
-  const { data: comp } = api.competition.getCompBySlug.useQuery({
-    slug: compSlug,
-  });
-  const { data: plan } = api.event.checkUserRegisteration.useQuery(
-    {
-      userId: CurrentUser?.id ?? "",
-      eventId: comp?.competitionDetailsId ?? "",
-    },
-    {
-      enabled: !!CurrentUser && !!comp,
-    },
-  );
-  const { data: regTeam } = api.team.findTeamOfUser.useQuery(
-    {
-      userId: CurrentUser?.id ?? "",
-      competitionId: comp?.id ?? "",
-    },
-    {
-      enabled: !!CurrentUser && !!comp,
-    },
-  );
+  const { data: comp, isLoading: isCompLoading } =
+    api.competition.getCompBySlug.useQuery({
+      slug: compSlug,
+    });
+  const { data: plan, isLoading: isRegistrationLoading } =
+    api.event.checkUserRegisteration.useQuery(
+      {
+        userId: CurrentUser?.id ?? "",
+        eventId: comp?.competitionDetailsId ?? "",
+      },
+      {
+        enabled: !!CurrentUser && !!comp,
+      },
+    );
+  const { data: regTeam, isLoading: isTeamLoading } =
+    api.team.findTeamOfUser.useQuery(
+      {
+        userId: CurrentUser?.id ?? "",
+        competitionId: comp?.id ?? "",
+      },
+      {
+        enabled: !!CurrentUser && !!comp,
+      },
+    );
 
   const regStatus = !!plan;
 
   return (
     <>
+      {(isCompLoading || isRegistrationLoading || isTeamLoading) && (
+        <div className="loading flex h-screen w-screen flex-col justify-center bg-neutral-900"></div>
+      )}
       {comp && (
         <div className="flex w-screen flex-col items-center justify-center gap-12 xl:flex-row">
           <CompetitionDetailsBox
