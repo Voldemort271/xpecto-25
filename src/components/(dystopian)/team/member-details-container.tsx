6e78b08a88717a5d52a9eb180d69b862fc96142e
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-01-30 16:38:06 +0530

    Added TODOs

--- src/components/(dystopian)/team/member-details-container.tsx
+++ src/components/(dystopian)/team/member-details-container.tsx
@@ -3,6 +3,8 @@ import React, { type ReactNode } from "react";
 const MemberDetailsContainer = ({ children }: { children: ReactNode }) => {
   return (
     <div className="h-full w-full bg-neutral-950 md:pt-16">
+      {/* TODO: Add "back to home" link */}
+      {/* TODO: Display controls at bottom right in a tech-like fashion */}
       <div className="relative h-full w-full">{children}</div>
     </div>
   );
