02c28afacd0bc6d85937919136605fd2f340b11b
Player <141478611+PLAYER1267@users.noreply.github.com>
2025-02-02 12:29:22 +0530

    Update page.tsx

--- src/app/(dystopian)/expos/page.tsx
+++ src/app/(dystopian)/expos/page.tsx
@@ -1,44 +1,38 @@
 "use client";
-import React, { useEffect } from "react";
-import { api } from "@/trpc/react"; // Import the api object
-import Link from "next/link";
-import SectionHeader from "@/components/(dystopian)/common/section-header";
-
+import React, { useEffect, useState } from "react";
+import { api } from "@/trpc/react";
+import ExposDetails from "@/components/(dystopian)/expos/expos-details";
+import ExposControl from "@/components/(dystopian)/expos/expos-details-control";
 
 const Page = () => {
-  const { data: expos } = api.expo.getExpo.useQuery();
+  const [index, setIndex] = useState(0);
+
+  const { data: expos, isLoading } = api.expo.getExpo.useQuery();
 
   useEffect(() => {
     if (expos) {
-      console.log("competition", expos);
+      console.log("expos", expos);
     }
   }, [expos]);
-
-
+  // TODO: Fix prop sharing to pass whole pronites object
   return (
-    <>
-    {/* //TODO: Add a searchbar for competitions */}
-    <SectionHeader title="Expos">Enjoy the expos</SectionHeader>
-      <div className="flex flex-col justify-center items-center gap-5">
-        {expos?.map((comp) => {
-          return (
-            <Link href={`/expos/${comp.exposDetails.slug}`} key={comp.id} className="flex gap-2 items-center border-2 p-2 rounded-lg bg-amber-50 text-neutral-900">
-              <div
-                style={{ backgroundImage: `url(/event_covers/competitions/${comp.exposDetails.slug}.jpeg), url(logo.enc)` }}
-                className="flex h-28 w-28 items-center justify-center bg-cover bg-no-repeat rounded-full"
-              >
-              </div>
-              <div className="bg-amber-50 w-1 h-28"></div>
-              <div>
-              <div className="font-bold text-lg">{comp.exposDetails.name}</div>
-              <div>{comp.exposDetails.begin_time.toString()}</div>
-              <div>{comp.exposDetails.description.slice(0, 51) + (comp.exposDetails.description.length > 50 ? "....." : "")}</div>
-              </div>
-            </Link>
-          );
-        })}
-      </div>
-    </>
+    <div className="relative z-0 h-full w-full overflow-clip">
+      {!isLoading && expos && expos[0] ? (
+        <ExposDetails
+          expos={expos[0]}
+          index={index}
+          length={10}
+          setIndex={setIndex}
+        >
+          {/* TODO BONUS: Host all images on Cloudinary to reduce bundle size drastically */}
+        </ExposDetails>
+      ) : (
+        <div className="loading h-full w-full"></div>
+      )}
+      {!isLoading && expos && expos[0] && (
+        <ExposControl expos={expos[0]} />
+      )}
+    </div>
   );
 };
 export default Page;
