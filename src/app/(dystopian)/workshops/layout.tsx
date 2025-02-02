0df29d6f7ec3b1f01d667fad46cd1c7e5bf49059
Player <141478611+PLAYER1267@users.noreply.github.com>
2025-02-02 15:47:59 +0530

    Update layout.tsx

--- src/app/(dystopian)/workshops/layout.tsx
+++ src/app/(dystopian)/workshops/layout.tsx
@@ -1,16 +1,30 @@
+"use client";
+
 import React from "react";
 import { Handjet } from "next/font/google";
+import WorkshopsHeader from "@/components/(dystopian)/workshops/workshops-header";
+import { usePathname } from "next/navigation";
 
 const handjet = Handjet({ subsets: ["latin"] });
 
 export default function ProfileLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const path = usePathname();
+
   return (
     <div
-      className={`relative flex h-screen w-screen border-2 border-amber-50 bg-neutral-900 text-amber-50 ${handjet.className} tracking-widest`}
+      className={
+        path === "/workshops"
+          ? `grid w-screen grid-rows-[400px_auto] overflow-clip bg-neutral-900 md:h-screen md:grid-cols-[200px_auto] md:grid-rows-1 lg:grid-cols-[300px_auto] xl:grid-cols-[350px_auto] ${handjet.className}`
+          : `grid w-screen grid-rows-[64px_auto] overflow-clip bg-neutral-900 md:h-screen md:grid-cols-[64px_auto] md:grid-rows-1 ${handjet.className}`
+      }
     >
-      <div className="mt-32 h-full w-full lg:mt-40">{children}</div>
+      <div className="relative h-full w-full">
+        <WorkshopsHeader />
+      </div>
+      <div className="relative h-full w-full bg-neutral-900">{children}</div>
     </div>
   );
 }
+
