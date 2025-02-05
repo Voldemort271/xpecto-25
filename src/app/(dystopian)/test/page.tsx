abd6bd0288fb2085fb026a8b361436bc5914dd88
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-02-06 00:28:38 +0530

    Duplicated carousel into competitions data, in /team

--- src/app/(dystopian)/test/page.tsx
+++ src/app/(dystopian)/test/page.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+"use client";
 
-const TestPage = () => {
-  return <div className="h-full w-full bg-red-400">hi</div>;
+import React, { useState } from "react";
+import TeamDetailsContainer from "@/components/(dystopian)/team/team-details-container";
+import TeamDetailsView from "@/components/(dystopian)/team/team-details-view";
+import { teamData } from "@/lib/team-data";
+import { api } from "@/trpc/react";
+import CompetitionsCarouselContainer from "@/components/(dystopian)/(test)/competitions-carousel-container";
+import CompetitionsCarousel from "@/components/(dystopian)/(test)/competitions-carousel";
+
+const TeamPage = () => {
+  const { data: competitions, isLoading } =
+    api.competition.getCompetitions.useQuery();
+
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div className="grid min-h-screen w-screen grid-rows-[600px_auto] overflow-clip bg-neutral-900 md:grid-cols-[400px_auto] md:grid-rows-1 lg:grid-cols-[600px_auto]">
+      {isLoading || !competitions || !competitions[index] ? (
+        <div className="loading h-full w-full border-2 border-amber-50 bg-neutral-900"></div>
+      ) : (
+        <CompetitionsCarouselContainer data={competitions[index]}>
+          <CompetitionsCarousel
+            data={competitions}
+            index={index}
+            setIndex={setIndex}
+          />
+        </CompetitionsCarouselContainer>
+      )}
+      <TeamDetailsContainer index={index} setIndex={setIndex}>
+        <TeamDetailsView data={teamData[index]} key={index} />
+      </TeamDetailsContainer>
+    </div>
+  );
 };
 
-export default TestPage;
+export default TeamPage;
---
+++ src/components/(dystopian)/(test)/competitions-carousel.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import {
+  Carousel,
+  type CarouselApi,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import React, {
+  type Dispatch,
+  type SetStateAction,
+  useEffect,
+  useState,
+} from "react";
+import Image from "next/image";
+import { type CompetitionWithDetails } from "@/app/types";
+import StaticImg from "public/images/img.png";
+
+interface Props {
+  data: CompetitionWithDetails[];
+  index: number;
+  setIndex: Dispatch<SetStateAction<number>>;
+}
+
+const CompetitionsCarousel = ({ data, index, setIndex }: Props) => {
+  const [apiDesktop, setApiDesktop] = useState<CarouselApi>();
+  const [apiMobile, setApiMobile] = useState<CarouselApi>();
+
+  const [pos, setPos] = useState(0);
+
+  useEffect(() => {
+    if (!apiDesktop) return;
+    apiDesktop.scrollTo(index);
+  }, [apiDesktop, index]);
+
+  useEffect(() => {
+    if (!apiMobile) return;
+    apiMobile.scrollTo(index);
+  }, [apiMobile, index]);
+
+  useEffect(() => {
+    if (!apiDesktop) return;
+    apiDesktop.on("select", () => {
+      setPos(apiDesktop.selectedScrollSnap());
+    });
+  }, [apiDesktop]);
+
+  useEffect(() => {
+    if (!apiMobile) return;
+    apiMobile.on("select", () => {
+      setPos(apiMobile.selectedScrollSnap());
+    });
+  }, [apiMobile]);
+
+  useEffect(() => {
+    setIndex(pos);
+  }, [pos, setIndex]);
+
+  return (
+    <div className="relative flex h-full w-full flex-col items-center justify-center px-5 lg:px-12">
+      <Carousel
+        setApi={setApiDesktop}
+        opts={{
+          align: "center",
+          loop: true,
+        }}
+        orientation="vertical"
+        className="hidden w-full md:block"
+      >
+        <CarouselContent className="-mt-1 h-[calc(100vh)]">
+          {data.map((el, index) => (
+            <CarouselItem
+              key={index}
+              className="relative flex flex-col items-center justify-center pt-5 md:basis-1/2"
+            >
+              <div className="relative h-full max-h-[400px] w-full border-2 border-amber-50 lg:max-h-[500px]">
+                <Image
+                  src={
+                    el.competitionDetails.cover === ""
+                      ? StaticImg
+                      : el.competitionDetails.cover
+                  }
+                  alt={el.competitionDetails.name}
+                  className="h-full w-full object-cover"
+                />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+      <Carousel
+        setApi={setApiMobile}
+        opts={{
+          align: "center",
+          loop: true,
+        }}
+        orientation="horizontal"
+        className="flex h-full w-full flex-col items-center justify-center py-5 md:hidden"
+      >
+        <CarouselContent className="h-full">
+          {data.map((el, index) => (
+            <CarouselItem
+              key={index}
+              className="relative h-full max-h-[300px] max-w-[400px] basis-3/4 pl-5"
+            >
+              <Image
+                src={
+                  el.competitionDetails.cover === ""
+                    ? StaticImg
+                    : el.competitionDetails.cover
+                }
+                alt={el.competitionDetails.name}
+                className="h-full w-full border-2 border-amber-50 object-cover"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </div>
+  );
+};
+
+export default CompetitionsCarousel;
