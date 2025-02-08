64beb1cae61dfce9cb5f794c49408bc363a98399
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-02-09 00:00:18 +0530

    Styled admin page

--- src/app/(root)/registrations/page.tsx
+++ src/app/(root)/registrations/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Image from "next/image";
 import { api } from "@/trpc/react";
 import { useCurrentUser } from "@/lib/utils";
+import { Share_Tech } from "next/font/google";
+import { CursorContext } from "@/context/cursor-context";
+
+const shareTech = Share_Tech({ weight: "400", subsets: ["latin"] });
 
 const Page = () => {
   const { CurrentUser } = useCurrentUser();
   const utils = api.useUtils();
+  const { setIsHovered } = useContext(CursorContext);
 
   const {
     data: regs,
@@ -16,7 +21,7 @@ const Page = () => {
   } = api.registration.getUnverifiedRegistrations.useQuery(
     CurrentUser?.clerkId ?? "",
   );
-  
+
   const [selectedReg, setSelectedReg] = useState<
     NonNullable<typeof regs>[number] | null
   >(null);
@@ -32,52 +37,68 @@ const Page = () => {
 
   const handleAccept = () => {
     if (!selectedReg) return;
-    verifyPayment.mutate({ registrationId: selectedReg.id }, {
-      onSuccess: (e) => {
-        setRejectionBox("");
-        setSelectedReg(null);
-        utils.registration.getUnverifiedRegistrations.setData(CurrentUser?.clerkId ?? "", prevReg => {
-          return prevReg?.filter(reg => reg.id !== e.id);
-        })
-      }
-    });
+    verifyPayment.mutate(
+      { registrationId: selectedReg.id },
+      {
+        onSuccess: (e) => {
+          setRejectionBox("");
+          setSelectedReg(null);
+          utils.registration.getUnverifiedRegistrations.setData(
+            CurrentUser?.clerkId ?? "",
+            (prevReg) => {
+              return prevReg?.filter((reg) => reg.id !== e.id);
+            },
+          );
+        },
+      },
+    );
   };
 
   const handleReject = () => {
     if (!selectedReg) return;
-    rejectPayment.mutate({ registrationId: selectedReg.id, reason: rejectionBox }, {
-      onSuccess: (e) => {
-        setRejectionBox("");
-        setSelectedReg(null);
-        utils.registration.getUnverifiedRegistrations.setData(CurrentUser?.clerkId ?? "", prevReg => {
-          return prevReg?.filter(reg => reg.id !== e.id);
-        })
-      }
-    });
+    rejectPayment.mutate(
+      { registrationId: selectedReg.id, reason: rejectionBox },
+      {
+        onSuccess: (e) => {
+          setRejectionBox("");
+          setSelectedReg(null);
+          utils.registration.getUnverifiedRegistrations.setData(
+            CurrentUser?.clerkId ?? "",
+            (prevReg) => {
+              return prevReg?.filter((reg) => reg.id !== e.id);
+            },
+          );
+        },
+      },
+    );
   };
 
   return (
-    <div>
+    <div className="bg-neutral-900">
       <div className="h-32"></div>
-      <div className="flex">
-        <div className="w-[64.75%] p-4">
-          <div className="flex flex-col gap-4">
+      <div
+        className={`grid grid-rows-[auto_auto] sm:grid-cols-[auto_400px] sm:grid-rows-1 ${shareTech.className} tracking-tight`}
+      >
+        <div className="p-4">
+          <div className="grid grid-cols-1 gap-2.5 lg:grid-cols-2 xl:grid-cols-3">
             {regs?.map((reg) => (
               <div
                 key={reg.id}
-                className={`mb-4 flex cursor-pointer border p-4 ${selectedReg?.id === reg.id ? "bg-amber-50 text-neutral-900" : ""}`}
+                className={`flex cursor-none border-2 border-amber-50 p-4 ${selectedReg?.id === reg.id ? "bg-amber-50/[0.5] text-neutral-900" : ""}`}
                 onClick={() => setSelectedReg(reg)}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
               >
-                <div className="details-container w-full pl-4">
-                  <div className="text-2xl">
-                    <strong>Transaction ID:</strong> {reg.paymentId}
-                  </div>
-                  <div className="text-2xl">
-                    Plan PRICE:<strong> Rs. {reg.plan.price}</strong>
-                  </div>
-                  <p>
+                <div className="details-container w-full pl-4 uppercase">
+                  <p className="text-2xl">
                     <strong>Name:</strong> {reg.user.name}
                   </p>
+                  <div className="text-xl">
+                    <strong>Transaction ID:</strong> {reg.paymentId}
+                  </div>
+                  <div className="text-xl">
+                    Plan PRICE:<strong> Rs. {reg.plan.price}</strong>
+                  </div>
                   <p>
                     <strong>College:</strong> {reg.user.college_name}
                   </p>
@@ -85,42 +106,35 @@ const Page = () => {
                     <strong>Plan:</strong> {reg.plan.name}
                   </p>
                   <p>
-                    <strong>Plan Details:</strong> {reg.plan.description}
-                  </p>
-                  <p>
                     <strong>Event:</strong> {reg.event.name}
                   </p>
-                  <p>
-                    <strong>Event Details:</strong> {reg.event.description}
-                  </p>
-                  <div className="flex items-center gap-4 mt-4">
+                  <div className="mt-4 flex flex-col items-center gap-4">
                     <button
-                      className="accept-button bg-green-500 px-4 py-2 text-white"
+                      className="accept-button w-full bg-green-500 px-4 py-2 text-white"
                       onClick={handleAccept}
                     >
                       Accept
                     </button>
                     <button
-                      className="reject-button bg-red-500 px-4 py-2 disabled:bg-red-100 text-white"
+                      className="reject-button w-full bg-red-500 px-4 py-2 text-white disabled:bg-red-100"
                       onClick={handleReject}
                       disabled={rejectionBox === ""}
                     >
                       Reject
                     </button>
                     <textarea
-                    className="border p-2 w-full"
-                    placeholder="Rejection reason (Compulsory to give something)"
-                    value={rejectionBox}
-                    onChange={(e) => setRejectionBox(e.target.value)}
-                  />
+                      className="w-full border p-2"
+                      placeholder="Rejection reason"
+                      value={rejectionBox}
+                      onChange={(e) => setRejectionBox(e.target.value)}
+                    />
                   </div>
                 </div>
               </div>
             ))}
           </div>
         </div>
-        <div className="w-[0.25%] bg-amber-50"></div>
-        <div className="w-[35%] p-4">
+        <div className="border-l-2 border-amber-50 p-4">
           {selectedReg ? (
             <Image
               src={`https://res.cloudinary.com/diqdg481x/image/upload/${selectedReg.paymentProof}`}
