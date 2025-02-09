7a763723abc1e92630572411e612a4ebeb3d047a
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-02-09 11:16:24 +0530

    Styled registrations page

--- src/app/(root)/registrations/page.tsx
+++ src/app/(root)/registrations/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Image from "next/image";
 import { api } from "@/trpc/react";
 import { useCurrentUser } from "@/lib/utils";
+import { Share_Tech } from "next/font/google";
+import { CursorContext } from "@/context/cursor-context";
+
+const sharetech = Share_Tech({ weight: "400", subsets: ["latin"] });
 
 const RejectionBox: React.FC<{ rejectFunc: (reason: string) => void }> = ({
   rejectFunc,
@@ -11,9 +15,9 @@ const RejectionBox: React.FC<{ rejectFunc: (reason: string) => void }> = ({
   const [rejectionBox, setRejectionBox] = useState("");
 
   return (
-    <div>
+    <>
       <button
-        className="reject-button bg-red-500 px-4 py-2 text-white disabled:bg-red-100"
+        className="reject-button w-full bg-red-500 px-4 py-2 text-white disabled:bg-red-100"
         onClick={() => {
           setRejectionBox("");
           rejectFunc(rejectionBox);
@@ -24,18 +28,19 @@ const RejectionBox: React.FC<{ rejectFunc: (reason: string) => void }> = ({
       </button>
       <textarea
         className="w-full border p-2"
-        placeholder="Rejection reason (Compulsory to give something)"
+        placeholder="Rejection reason"
         value={rejectionBox}
         onChange={(e) => setRejectionBox(e.target.value)}
       />
-    </div>
+    </>
   );
 };
 //This rejection box was just made for dynamism
 
-const Page = () => {
+const ApprovalPage = () => {
   const { CurrentUser } = useCurrentUser();
   const utils = api.useUtils();
+  const { setIsHovered } = useContext(CursorContext);
 
   const {
     data: regs,
@@ -94,58 +99,52 @@ const Page = () => {
   };
 
   return (
-    <div>
+    <div className={`bg-neutral-900 ${sharetech.className} tracking-tight`}>
       <div className="h-32"></div>
-      <div className="flex">
-        <div className="w-[64.75%] p-4">
-          <div className="flex flex-col gap-4">
-            {regs?.map((reg) => (
-              <div
-                key={reg.id}
-                className={`mb-4 flex cursor-pointer border p-4 ${selectedReg?.id === reg.id ? "bg-amber-50 text-neutral-900" : ""}`}
-                onClick={() => setSelectedReg(reg)}
-              >
-                <div className="details-container w-full pl-4">
-                  <div className="text-2xl">
-                    <strong>Transaction ID:</strong> {reg.paymentId}
-                  </div>
-                  <div className="text-2xl">
-                    Plan PRICE:<strong> Rs. {reg.plan.price}</strong>
-                  </div>
-                  <p>
-                    <strong>Name:</strong> {reg.user.name}
-                  </p>
-                  <p>
-                    <strong>College:</strong> {reg.user.college_name}
-                  </p>
-                  <p>
-                    <strong>Plan:</strong> {reg.plan.name}
-                  </p>
-                  <p>
-                    <strong>Plan Details:</strong> {reg.plan.description}
-                  </p>
-                  <p>
-                    <strong>Event:</strong> {reg.event.name}
-                  </p>
-                  <p>
-                    <strong>Event Details:</strong> {reg.event.description}
-                  </p>
-                  <div className="mt-4 flex items-center gap-4">
-                    <button
-                      className="accept-button bg-green-500 px-4 py-2 text-white"
-                      onClick={handleAccept}
-                    >
-                      Accept
-                    </button>
-                    <RejectionBox rejectFunc={handleReject} />
-                  </div>
+      <div className="grid grid-rows-[auto_auto] sm:grid-cols-[auto_400px] sm:grid-rows-1">
+        <div className="grid grid-cols-1 gap-2.5 p-4 lg:grid-cols-2 xl:grid-cols-3">
+          {regs?.map((reg) => (
+            <div
+              key={reg.id}
+              className={`flex cursor-none border-2 border-amber-50 p-4 ${selectedReg?.id === reg.id ? "bg-amber-50/[0.5] text-neutral-900" : ""}`}
+              onClick={() => setSelectedReg(reg)}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              <div className="details-container w-full">
+                <p className="text-xl">
+                  <strong>Name:</strong> {reg.user.name}
+                </p>
+                <div className="text-2xl">
+                  <strong className="text-lg">Transaction ID:</strong>{" "}
+                  {reg.paymentId}
+                </div>
+                <div className="text-2xl">
+                  Plan:
+                  <strong className="ml-2">
+                    {reg.plan.name.toUpperCase()} - Rs. {reg.plan.price}
+                  </strong>
+                </div>
+                <p>
+                  <strong>College:</strong> {reg.user.college_name}
+                </p>
+                <p>
+                  <strong>Event:</strong> {reg.event.name}
+                </p>
+                <div className="mt-4 flex flex-col items-center gap-4">
+                  <button
+                    className="accept-button w-full bg-green-500 px-4 py-2 text-white"
+                    onClick={handleAccept}
+                  >
+                    Accept
+                  </button>
+                  <RejectionBox rejectFunc={handleReject} />
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
-        <div className="w-[0.25%] bg-amber-50"></div>
-        <div className="w-[35%] p-4">
+        <div className="border-l border-amber-50/[0.5] p-4">
           {selectedReg ? (
             <Image
               src={`https://res.cloudinary.com/diqdg481x/image/upload/${selectedReg.paymentProof}`}
@@ -163,4 +162,4 @@ const Page = () => {
   );
 };
 
-export default Page;
+export default ApprovalPage;
