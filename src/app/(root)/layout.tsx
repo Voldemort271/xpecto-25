476aaac1190470cf016bf5bfca813a9424716331
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-02-09 18:59:12 +0530

    Added TODO

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
   }, [pathname, setIsHovered]);
 
   return (
+    // TODO: Clear all console.logs
     <CursorProvider>
       <div
         className={`relative h-screen w-screen cursor-none bg-neutral-900 text-amber-50 ${handjet.className} tracking-widest`}
