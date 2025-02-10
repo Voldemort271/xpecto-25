e42dd848b829bfb70a6045430bec6cf02dcd54d4
harshpandey <harshpandeyiitian04@gmail.com>
2025-02-11 03:23:00 +0530

    replication work for expo , pronite and workshop

--- src/components/expos/expos-header.tsx
+++ src/components/expos/expos-header.tsx
@@ -2,13 +2,21 @@
 
 import React from "react";
 import MarqueeContainer from "@/components/common/marquee-container";
+import Image from "next/image";
 import { motion } from "motion/react";
 
 const ExposHeader = () => {
   return (
-    <div className="h-full w-full bg-neutral-900">
+    <div className="relative flex h-full w-full flex-row gap-0 bg-neutral-900">
+      <Image
+        src={`https://res.cloudinary.com/diqdg481x/image/upload/v1737737277/background_eqguit.jpg`} // Changed the source of the background image
+        width={300}
+        height={300} // Added sample width and height coz it was giving an error
+        alt={"Bg Image"}
+        className="absolute left-0 top-0 h-full w-full object-cover object-center opacity-50"
+      />
       <motion.div
-        className="absolute left-0 top-[126px] z-30 flex h-16 w-screen flex-col justify-center overflow-clip border-2 border-amber-50 bg-neutral-900 text-3xl font-light uppercase md:top-0 md:w-[100vh] md:-translate-x-[calc(50%-32px)] md:translate-y-[calc(50vh-32px)] md:-rotate-90"
+        className="absolute left-0 top-[126px] flex h-16 w-screen flex-col justify-center overflow-clip border-2 border-amber-50 bg-neutral-900 text-3xl font-light uppercase md:top-0 md:w-[100vh] md:-translate-x-[calc(50%-32px)] md:translate-y-[calc(50vh-32px)] md:-rotate-90"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
@@ -32,3 +40,37 @@ const ExposHeader = () => {
 };
 
 export default ExposHeader;
+// "use client";
+
+// import React from "react";
+// import MarqueeContainer from "@/components/common/marquee-container";
+// import { motion } from "motion/react";
+
+// const ExposHeader = () => {
+//   return (
+//     <div className="h-full w-full bg-neutral-900">
+//       <motion.div
+//         className="absolute left-0 top-[126px] z-30 flex h-16 w-screen flex-col justify-center overflow-clip border-2 border-amber-50 bg-neutral-900 text-3xl font-light uppercase md:top-0 md:w-[100vh] md:-translate-x-[calc(50%-32px)] md:translate-y-[calc(50vh-32px)] md:-rotate-90"
+//         initial={{ opacity: 0 }}
+//         animate={{ opacity: 1 }}
+//         transition={{ duration: 0.5 }}
+//       >
+//         <MarqueeContainer
+//           text={[
+//             "Expos",
+//             "xpecto '25",
+//             "iit mandi",
+//             "Expos",
+//             "xpecto '25",
+//             "iit mandi",
+//             "Expos",
+//             "xpecto '25",
+//             "iit mandi",
+//           ]}
+//         />
+//       </motion.div>
+//     </div>
+//   );
+// };
+
+// export default ExposHeader;
--- src/components/pronites/pronite-details-container.tsx
+++ src/components/pronites/pronite-details-container.tsx
@@ -81,3 +81,86 @@ const ProniteDetailsContainer = ({
 };
 
 export default ProniteDetailsContainer;
+// "use client";
+
+// import React, {
+//   type Dispatch,
+//   type ReactNode,
+//   type SetStateAction,
+//   useContext,
+// } from "react";
+// import { motion } from "motion/react";
+// import { CursorContext } from "@/context/cursor-context";
+// import { ProniteWithDetails } from "@/app/types";
+
+// interface Props {
+//   index: number;
+//   setIndex: Dispatch<SetStateAction<number>>;
+//   children: ReactNode;
+//   data: ProniteWithDetails[];
+// }
+
+// const keyframes = {
+//   flicker: {
+//     opacity: [0, 1, 0.3, 0.7, 0.3, 1, 0.3, 1, 0.3, 1, 0.3, 1],
+//   },
+// };
+
+// const ProniteDetailsContainer = ({
+//   index,
+//   setIndex,
+//   children,
+//   data,
+// }: Props) => {
+//   const { setIsHovered } = useContext(CursorContext);
+
+//   return (
+//     <div className="h-full w-full bg-neutral-900 md:pt-16">
+//       <div className="relative flex h-full w-full flex-col items-start justify-between md:pt-12">
+//         {children}
+//         <div className="z-0 grid min-w-72 grid-cols-2 p-2.5 pb-12 sm:p-5">
+//           <motion.div
+//             className="col-span-2 text-2xl font-light uppercase text-amber-50/[0.8]"
+//             variants={keyframes}
+//             animate="flicker"
+//             transition={{
+//               duration: 1,
+//               ease: "linear",
+//               repeat: 1,
+//             }}
+//           >
+//             navigate realm
+//           </motion.div>
+//           {data
+//             .slice(
+//               index < 2 ? index : index - 2,
+//               index < 2 ? index + 5 : index + 3,
+//             )
+//             .map((el, i) => (
+//               <motion.div
+//                 className="pr-2.5 text-lg font-light uppercase text-amber-50/[0.7] transition-all hover:font-light md:font-extralight"
+//                 key={i}
+//                 onClick={() =>
+//                   setIndex((prev) => (prev < 2 ? prev + i : prev - 2 + i))
+//                 }
+//                 onMouseEnter={() => setIsHovered(true)}
+//                 onMouseLeave={() => setIsHovered(false)}
+//                 variants={keyframes}
+//                 animate="flicker"
+//                 transition={{
+//                   duration: 1,
+//                   ease: "easeInOut",
+//                   repeat: 1,
+//                   delay: 0.5 + i * 0.2,
+//                 }}
+//               >
+//                 {el.proniteDetails.name}
+//               </motion.div>
+//             ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProniteDetailsContainer;
