2fb03d55d1801bffa0e7351d713127ae9e00c9ae
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-02-14 12:24:31 +0530

    Added loading screen to /team

--- src/app/(root)/team/page.tsx
+++ src/app/(root)/team/page.tsx
@@ -6,6 +6,7 @@ import TeamCarousel from "@/components/team/team-carousel";
 import TeamDetailsContainer from "@/components/team/team-details-container";
 import TeamDetailsView from "@/components/team/team-details-view";
 import { api } from "@/trpc/react";
+import MarqueeContainer from "@/components/common/marquee-container";
 
 const TeamPage = () => {
   const [index, setIndex] = useState(0);
@@ -15,7 +16,26 @@ const TeamPage = () => {
   //TODO: Loaders needed all over the code everywhere required wherever useQuery or useMutation were used. Super annoying.
 
   return isLoading ? (
-    <div>Loading</div>
+    <div className="loading relative z-0 h-screen w-screen bg-neutral-900">
+      <div className="absolute left-1/2 top-1/2 flex h-16 w-screen -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center overflow-clip border-2 border-amber-50 bg-neutral-900 text-4xl font-normal uppercase text-amber-50">
+        <MarqueeContainer
+          text={[
+            "Loading",
+            "hang in there",
+            "loading squad",
+            "hang in there",
+            "Loading",
+            "hang in there",
+            "loading squad",
+            "hang in there",
+            "Loading",
+            "hang in there",
+            "loading squad",
+            "hang in there",
+          ]}
+        />
+      </div>
+    </div>
   ) : (
     teamData && (
       <div className="grid min-h-screen w-screen grid-rows-[600px_auto] overflow-clip bg-neutral-900 md:grid-cols-[400px_auto] md:grid-rows-1 lg:grid-cols-[600px_auto]">
