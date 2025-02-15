8eecd19bb374c62067a97bb060f51dfa968ccc5a
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-02-15 13:58:25 +0530

    Removed TODO

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -11,7 +11,6 @@ const Home = () => {
   return (
     <div className="flex h-full w-full flex-col items-center bg-neutral-900">
       <HomeScreen />
-      {/* TODO: Change text */}
       <div className="flex h-32 w-full flex-col justify-center overflow-hidden whitespace-nowrap border-y-2 border-amber-50 bg-neutral-950 text-9xl font-extrabold uppercase text-amber-50/[0.8]">
         <MarqueeContainer text={["29 to 31 march", "iit mandi techfest"]} />
       </div>
