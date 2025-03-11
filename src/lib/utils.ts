7e85cff7ff2e8b20d3a8235d3408c77e15909ce4
mrhello291 <hoda.asif123@gmail.com>
2025-03-11 16:42:46 +0530

    fixed an issue with datetime in featuredevents

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -52,11 +52,8 @@ export const getLowestPrice = (regPlans: { price: number }[]) => {
 };
 
 export const formatDateToHour = (date: Date) => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const hour = String(date.getHours()).padStart(2, "0");
-  return `${year}-${month}-${day}T${hour}:00:00.000Z`;
+  const isoString = date.toISOString();
+  return isoString.slice(0, 13) + ":00:00.000Z";
 };
 
 
