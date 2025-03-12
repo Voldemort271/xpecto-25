d96d48e1950e3c47793a4c79480d8fd478a19b9b
Shubham S Padhi <shubhamspadhi@gmail.com>
2025-03-13 00:04:18 +0530

    Removed animation delay on time machine

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -16,7 +16,7 @@ const DystopianNav = () => {
   const { CurrentUser, isLoading } = useCurrentUser();
   const { setIsHovered } = useContext(CursorContext);
   const path = usePathname();
-  const animationDelay = path === "/" ? 6 : 0;
+  const animationDelay = 0;
 
   // Extract userId from CurrentUser
   const loggedInUserId = CurrentUser?.id ?? ""; // Default to empty string if not logged in
